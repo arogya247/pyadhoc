907b7cbde58b6566fe8ebe473381d11f9b2fbf1f
arogya247 <41734737+arogya247@users.noreply.github.com>
2018-09-15 15:08:04 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,17 +2,34 @@ import socket,commands
 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-#ip="127.0.0.1"
-port=7890
+ip="127.0.0.1"
+port1=7890
+port2=8888
 
-s.bind(("",port))
-while True:
+s.bind(("",port2))
+
+user=s.recvfrom(20)
+password=s.recvfrom(20)
+print (user[0])
+print (password[0])
+
+if ("root") in user and ("123") in password:
+	msg1=("Correct")
+	s.sendto(msg1,(ip,port1))
+	print ("OK")
+	while True:
+		data=s.recvfrom(20)
+		recv_cmd=data[0]
+		d=commands.getoutput(recv_cmd)
+		print (d)
+else:
+	msg=("wrong")
+	s.sendto(msg,(ip,port1))
+	print ("INVALID PASSWORD OR USERNAME..!!")
+	exit()
 	
-	data=s.recvfrom(20)
-	recv_cmd=data[0]
-	d=commands.getoutput(recv_cmd)
-	print (d)
-
+		
+	
 
 	
 
