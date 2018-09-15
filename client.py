b0143ac85ff290890367ee3e46430d3f34e62732
arogya247 <41734737+arogya247@users.noreply.github.com>
2018-09-15 15:09:01 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,16 +3,28 @@ import socket,commands
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 ip="127.0.0.1"
-port=7890
+port1=7890
+port2=8888
 
-while True:
-	cmd=raw_input()
-	a=commands.getoutput(cmd)
-	if "not found" in a:
-		print ("Invalid Command")
-		continue
-		
-	s.sendto(cmd,(ip,port))
+s.bind(("",port1))
+
+user=raw_input("Enter Username:  ")
+password=raw_input("Enter Password:  ")
+s.sendto(user,(ip,port2))
+s.sendto(password,(ip,port2))
+
+received=s.recvfrom(20)
+if ("wrong") in received[0]:
+	print ("invalid")
+	exit()
+elif ("Correct") in received[0]:
+	while True:
+		cmd=raw_input("Enter Command:  ")
+		a=commands.getoutput(cmd)
+		if "not found" in a:
+			print ("Invalid Command")
+			continue
+		s.sendto(cmd,(ip,port2))
 	
 
 	
