761c5a0799e64147fe43ec6eebbe25d8ff2d3e98
arogya247 <41734737+arogya247@users.noreply.github.com>
2018-09-20 05:02:57 +0530

    Update cpy_cmd.py

--- cpy_cmd.py
+++ cpy_cmd.py
@@ -1,24 +1,62 @@
+#! /usr/bin/python3
+import os.path
+from os import path 
 import sys
 import pathlib
 
 src_file=sys.argv[1]
-dst_file=sys.argv[2]
-'''
-#creating source file
-f=open(src_file,"w")
-f.write("hello how are you")
-'''
+dst_file=sys.argv[2:]
 
-file=pathlib.Path(src_file)
-if file.exists():
-#opening source file
-	f=open(src_file,"r")
-	src_data=f.read()
-	f.close()
+if sys.argv[2]==("-v"):
+	
+	dst_file=sys.argv[3:]
+	#check if file exists
+	file=pathlib.Path(src_file)
+	if file.exists():
+		
+		#check if src is a file
+		path=src_file
+		if os.path.isfile(path):
 
-#copying into destination file
-	f1=open(dst_file,"w")
-	f1.write(src_data)
-	f1.close()
+			#opening source file
+			f=open(src_file,"r")
+			src_data=f.read()
+			f.close()
+
+			#copying into destination file
+			for i in dst_file:
+				f1=open(i,"w")
+				f1.write(src_data)
+				f1.close()
+				print(src_file + "----->" + i)
+		else:
+			print("Source File is not a file..!")
+
+	else:
+		print ("File does not exist..!")
+
 else:
-	print ("File does not exist..!")
+	#check if file exists
+	file=pathlib.Path(src_file)
+	if file.exists():
+		
+		#check if src is a file
+		path=src_file
+		if os.path.isfile(path):
+
+			#opening source file
+			f=open(src_file,"r")
+			src_data=f.read()
+			f.close()
+
+			#copying into destination file
+			for i in dst_file:
+				f1=open(i,"w")
+				f1.write(src_data)
+				f1.close()
+		else:
+			print("Source File is not a file..!")
+
+	else:
+		print ("File does not exist..!")
+
